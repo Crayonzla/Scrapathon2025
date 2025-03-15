93aed6082234e22c116689f69d7bffa7ccb44fc9
Crayonzla <98232931+Crayonzla@users.noreply.github.com>
2025-03-15 14:42:30 -0700

    sdasd

--- scrapathon-2025/Scripts/fish.gd
+++ scrapathon-2025/Scripts/fish.gd
@@ -3,30 +3,46 @@ extends CharacterBody2D
 var current_point: Vector2
 var target_point: Vector2
 var ttarget_point: Vector2
-var can_move = false
+var total_time = 200
+var percent_left: int
+var difference: Vector2
 
+var can_move = true
+
+@export var time_left = 200
+@export var decay_rate = 0.01
 @export var speed = 200
-@export var target_distance_min_max = Vector2(-10,10)
+@export var target_distance_min_max = Vector2(-1000,1000)
+@export var timer_minmax = Vector2(0,5)
 
 @onready var timer1 = $Timer1
 
 func _ready() -> void:
-	timer1.start()
 	check_target_point()
 
 func _process(delta: float) -> void:
-	current_point = self.position
+	evolve()
+	
 	if can_move:
-		var direction = (target_point-current_point).normalized()
+		var direction = (target_point - current_point).normalized()
 		velocity = (direction * speed)
+		var temp_current_point = self.global_position
+		difference = abs(Vector2(temp_current_point.x-target_point.x, temp_current_point.y-target_point.y))
+		
+		move_and_slide()
+		
+		if difference.x < 10 or difference.y < 10 :
+			can_move = false
+			var time = randi_range(timer_minmax.x, timer_minmax.y)
+			timer1.start(time)
 	
-	if target_point == current_point:
-		can_move = false
+	percent_left = (time_left/total_time)*100
+	print(percent_left)
 	
-	move_and_slide()
-
 func check_target_point():
 	var target_distance_xy: Vector2
+	current_point = self.global_position
+	
 	target_distance_xy.x = randi_range(target_distance_min_max.x, target_distance_min_max.y)
 	target_distance_xy.y = randi_range(target_distance_min_max.x, target_distance_min_max.y)
 	ttarget_point = Vector2((current_point.x + target_distance_xy.x), (current_point.y + target_distance_xy.y))
@@ -34,8 +50,19 @@ func check_target_point():
 
 func get_target_point():
 	var ws = Global.window_size
-	print(ttarget_point)
 	if ttarget_point.x < 0 or ttarget_point.x > ws.x or ttarget_point.y > ws.y or ttarget_point.y < 0:
 		check_target_point()
 	else:
 		target_point = ttarget_point
+		can_move = true
+
+func evolve():
+	total_time += decay_rate
+	if total_time == 0:
+		pass
+
+func animation_manager():
+	pass
+
+func _on_timer_1_timeout() -> void:
+	check_target_point()
--- scrapathon-2025/Scripts/root.gd
+++ scrapathon-2025/Scripts/root.gd
@@ -6,11 +6,7 @@ func _ready() -> void:
 	window_size = DisplayServer.screen_get_size()
 	DisplayServer.window_set_size(window_size)
 	get_window().mouse_passthrough = true
-	var window = get_window()
-	window.mode = Window.MODE_EXCLUSIVE_FULLSCREEN
-	await get_tree().process_frame
-	window.mode = Window.MODE_WINDOWED
-	window.always_on_top = true
 
 func _process(delta: float) -> void:
 	get_window().always_on_top = true
+	
